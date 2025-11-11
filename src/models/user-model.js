bc8410513159278712ad486c11bf37b4c11025f4
rushdy87 <mohammed_alrushdy@yahoo.com>
2025-11-12 00:03:26 +0300

    Create user model

---
+++ src/models/user-model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
